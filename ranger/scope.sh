cb7313a610c16f29a43706ee223e1f22335dfe89
Shotaro Fujimoto <fuji101ijuf@gmail.com>
2015-02-04 01:31:13 +0900

    modify

--- ranger/scope.sh
+++ ranger/scope.sh
@@ -75,10 +75,10 @@ case "$mimetype" in
     image/*)
         img2txt --gamma=0.6 --width="$width" "$path" && exit 4 || exit 1;;
     # Display information about media files:
-    video/* | audio/*)
-        exiftool "$path" && exit 5
+    # video/* | audio/*)
+        # exiftool "$path" && exit 5
         # Use sed to remove spaces so the output fits into the narrow window
-        try mediainfo "$path" && { dump | trim | sed 's/  \+:/: /;';  exit 5; } || exit 1;;
+        # try mediainfo "$path" && { dump | trim | sed 's/  \+:/: /;';  exit 5; } || exit 1;;
 esac
 
 exit 1
