86eed4bbb87cca86e5625dc8959da72b7d9bd5d1
Shotaro Fujimoto <fuji101ijuf@gmail.com>
2015-05-03 21:31:38 +0900

    zsh files

---
+++ aliases.zsh
@@ -0,0 +1,34 @@
+# Push and pop directories on directory stack
+#alias pu='pushd'
+#alias po='popd'
+
+# Basic directory operations
+alias ...='cd ../..'
+alias -- -='cd -'
+
+# Super user
+alias _='sudo'
+alias sudo='sudo '
+#alias please='sudo'
+
+# Show history
+if [ "$HIST_STAMPS" = "mm/dd/yyyy" ]
+then
+    alias history='fc -fl 1'
+elif [ "$HIST_STAMPS" = "dd.mm.yyyy" ]
+then
+    alias history='fc -El 1'
+elif [ "$HIST_STAMPS" = "yyyy-mm-dd" ]
+then
+    alias history='fc -il 1'
+else
+    alias history='fc -l 1'
+fi
+# List direcory contents
+alias lsa='ls -lah'
+alias l='ls -lah'
+alias ll='ls -lh'
+alias la='ls -lAh'
+
+alias afind='ack-grep -il'
+
