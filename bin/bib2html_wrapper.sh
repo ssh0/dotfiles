97eb3fda27f34764296c01c13ee8132472a7ae73
ssh0 <fuji101ijuf@gmail.com>
2015-05-29 20:41:36 +0900

    add: bibtex2html wrapper script

---
+++ bin/bib2html_wrapper.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# written by Shotaro Fujimoto (https://github.com/ssh0)
+# first edited: 2015-05-29
+
+style='junsrt'
+bibtex_command='upbibtex'
+css="$HOME/Workspace/blog/styles/bootstrap-md.css"
+
+bibtex2html -s "${style}" -c "${bibtex_command}" -css "${css}" $@
