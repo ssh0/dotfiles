26d6eb2af91faba56a701e227aa6f4c55fc36609
Shotaro Fujimoto <fuji101ijuf@gmail.com>
2015-06-14 20:13:27 +0900

    change duration

--- bin/wallpaperchanger.sh
+++ bin/wallpaperchanger.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 picture_folder=/media/shotaro/STOCK/Pictures/wallpapers
-sleeptime=15m
+sleeptime=1m
 
 while true; do
   for file in $(ls $picture_folder); do
     feh --bg-fill "${picture_folder}/${file}"
     echo ${picture_folder}/${file}
-    sleep $sleeptime
+    sleep "$sleeptime"
   done
 done
