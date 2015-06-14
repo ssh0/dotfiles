1bc82ae1aa9c680a18133001d33a695d1892694c
Shotaro Fujimoto <fuji101ijuf@gmail.com>
2015-06-14 16:13:09 +0900

    start_urxvtd.sh

---
+++ bin/start_urxvtd.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# written by Shotaro Fujimoto (https://github.com/ssh0)
+# first edited: 2015-06-14
+
+if [ "$(ps -aux | grep urxvtd | grep -v grep)" = "" ]; then
+  urxvtd -q -f -o
+  echo "urxvt damon is started."
+else
+  echo "urxvt damon has been already started."
+fi
+
+exit 0
