08c333874afd55eb9ef50f6bc99a7bae42aa9e76
Shotaro Fujimoto <fuji101ijuf@gmail.com>
2015-06-14 22:30:38 +0900

    modified

--- zshfiles/aliases.mine.zsh
+++ zshfiles/aliases.mine.zsh
@@ -50,22 +50,22 @@ function _mkdocs() {
 # extract images {{{
 #------------------------------------------------------
 function extract() {
-    case $1 in
-        *.tar.gz|*.tgz) tar xzvf $1;;
-        *.tar.xz) tar Jxvf $1;;
-        *.zip) unzip $1;;
-        *.lzh) lha e $1;;
-        *.tar.bz2|*.tbz) tar xjvf $1;;
-        *.tar.Z) tar zxvf $1;;
-        *.gz) gzip -d $1;;
-        *.bz2) bzip2 -dc $1;;
-        *.Z) uncompress $1;;
-        *.tar) tar xvf $1;;
-        *.arj) unarj $1;;
-    esac
+  case $1 in
+    *.tar.gz|*.tgz) tar xzvf $1;;
+    *.tar.xz) tar Jxvf $1;;
+    *.zip) unzip $1;;
+    *.lzh) lha e $1;;
+    *.tar.bz2|*.tbz) tar xjvf $1;;
+    *.tar.Z) tar zxvf $1;;
+    *.gz) gzip -d $1;;
+    *.bz2) bzip2 -dc $1;;
+    *.Z) uncompress $1;;
+    *.tar) tar xvf $1;;
+    *.arj) unarj $1;;
+  esac
 }
 alias -s {qz,tgz,zip,lzh,bz2,tbz,Z,tar,arj,xz}=extract
-alias -s {png,jpg,bmp,PNG,JPG,BMP}='eog'
+alias -s {png,jpg,bmp,PNG,JPG,BMP}='feh -g 1440x900 -Z "$@"'
 # }}}
 # takenote completion {{{
 #
