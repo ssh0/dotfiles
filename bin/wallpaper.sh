5464981c436522abccfc3ff9a8f6a08d32449bd2
Shotaro Fujimoto <fuji101ijuf@gmail.com>
2015-06-14 22:30:25 +0900

    modified

--- bin/wallpaper.sh
+++ bin/wallpaper.sh
@@ -2,11 +2,13 @@
 # written by Shotaro Fujimoto (https://github.com/ssh0)
 # first edited: 2015-06-14
 
-WALLPAPERS_PATH=$HOME/.mywallpapers
+# use ~/.fehbg
+WALLPAPERS_PATH=$HOME/.fehbg
+
 if [ ! -e "${WALLPAPERS_PATH}" ]; then
   echo "${WALLPAPERS_PATH} doesn't exist."
   exit 1
 fi
 
-feh --bg-fill "$(tail -n 1 "${WALLPAPERS_PATH}")"
+feh --bg-fill "$(tail -n 1 "${WALLPAPERS_PATH}" | awk '{print $3;}')"
 exit 0
