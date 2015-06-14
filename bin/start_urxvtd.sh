bcbf50f6e3de0a425d5c1b1229ad81ac01576046
Shotaro Fujimoto <fuji101ijuf@gmail.com>
2015-06-14 16:18:45 +0900

    bug fixed:

--- bin/start_urxvtd.sh
+++ bin/start_urxvtd.sh
@@ -2,7 +2,8 @@
 # written by Shotaro Fujimoto (https://github.com/ssh0)
 # first edited: 2015-06-14
 
-if [ "$(ps -aux | grep urxvtd | grep -v grep)" = "" ]; then
+condition="$(ps -aux | grep urxvtd | grep -v grep | grep -v $0)" 
+if [ "$condition" = "" ]; then
   urxvtd -q -f -o
   echo "urxvt damon is started."
 else
