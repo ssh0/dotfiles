8b20995dff0e85285da1846e220cffe68a5add78
ssh0 <fuji101ijuf@gmail.com>
2015-06-19 22:51:33 +0900

    added trackpoint_toggle.sh

---
+++ bin/trackpoint_toggle.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# written by Shotaro Fujimoto (https://github.com/ssh0)
+# first edited: 2015-06-19
+
+declare -i ID
+ID=`xinput list | grep -Eo 'TrackPoint\s*id\=[0-9]{1,2}' | grep -Eo '[0-9]{1,2}'`
+declare -i STATE
+STATE=`xinput list-props $ID | grep 'Device Enabled' | awk '{print $4}'`
+if [ $STATE -eq 1 ]; then
+    xinput disable $ID
+    notify-send "TrackPoint disabled." \
+        -t 1000
+else
+    xinput enable $ID
+    notify-send "TrackPoint enabled." \
+        -t 1000
+fi
