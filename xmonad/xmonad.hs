cb7b74e25c8649121a35a9dde0b7712d2ff98e79
Shotaro Fujimoto <fuji101ijuf@gmail.com>
2015-08-18 23:00:47 +0900

    modified: xmonad.hs
    add comment & remove unused features

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -22,7 +22,7 @@ import XMonad.Actions.WindowGo
 import XMonad.Actions.FloatKeys
 import XMonad.Actions.CycleWS
 import XMonad.Actions.UpdatePointer
-import qualified XMonad.Actions.FlexibleResize as Flex -- Resize floating windows from any corner
+import qualified XMonad.Actions.FlexibleResize as Flex -- flexible resize
 import XMonad.Hooks.DynamicLog         -- for xmobar
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.FadeWindows
@@ -30,15 +30,11 @@ import XMonad.Hooks.ManageDocks        -- avoid xmobar area
 import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.Place
 import XMonad.Layout
--- import XMonad.Layout.DecorationMadness
 import XMonad.Layout.DragPane          -- see only two window
 import XMonad.Layout.Gaps
-import XMonad.Layout.Grid
-import XMonad.Layout.IM
-import qualified XMonad.Layout.Magnifier as Mag  -- this makes window bigger
+-- import qualified XMonad.Layout.Magnifier as Mag  -- this makes window bigger
 import XMonad.Layout.MultiToggle
 import XMonad.Layout.MultiToggle.Instances
--- import XMonad.Layout.Named
 import XMonad.Layout.NoBorders         -- In Full mode, border is no use
 import XMonad.Layout.PerWorkspace      -- Configure layouts on a per-workspace
 import XMonad.Layout.ResizableTile     -- Resizable Horizontal border
@@ -82,10 +78,6 @@ gapWidthU = 24
 gapWidthD = 24
 gapWidthL = 13
 gapWidthR = 13
--- gapWidthU = 17
--- gapWidthD = 17
--- gapWidthL = 13
--- gapWidthR = 13
 
 --------------------------------------------------------------------------- }}}
 -- Define keys to remove                                                    {{{
@@ -139,36 +131,49 @@ main = do
        ------------------------------------------------------------------------
 
        `additionalKeys`
-       [ ((modm                , xK_h      ), sendMessage Shrink)
+       [
+       -- Shrink / Expand the focused window
+         ((modm                , xK_h      ), sendMessage Shrink)
        , ((modm                , xK_l      ), sendMessage Expand)
        , ((modm                , xK_z      ), sendMessage MirrorShrink)
        , ((modm                , xK_a      ), sendMessage MirrorExpand)
-       , ((modm                , xK_c      ), kill) -- Close the focused window
+       -- Close the focused window
+       , ((modm                , xK_c      ), kill)
+       -- Toggle layout (Fullscreen mode)
        , ((modm                , xK_f      ), sendMessage ToggleLayout)
+       -- Move the focused window when the border width is none zero
        , ((modm .|. shiftMask  , xK_f      ), withFocused (keysMoveWindow (-borderwidth, -borderwidth)))
+       -- Move the focused window
        , ((modm .|. controlMask, xK_Right  ), withFocused (keysMoveWindow (6,0)))
        , ((modm .|. controlMask, xK_Left   ), withFocused (keysMoveWindow (-6,0)))
        , ((modm .|. controlMask, xK_Up     ), withFocused (keysMoveWindow (0,-6)))
        , ((modm .|. controlMask, xK_Down   ), withFocused (keysMoveWindow (0,6)))
-       , ((modm                , xK_s      ), withFocused (keysResizeWindow (-12,-12) (0.5,0.5))) -- shrink window
-       , ((modm                , xK_i      ), withFocused (keysResizeWindow (12,12) (0.5,0.5))) -- increase window
+       -- Resize the focused window
+       , ((modm                , xK_s      ), withFocused (keysResizeWindow (-12,-12) (0.5,0.5)))
+       , ((modm                , xK_i      ), withFocused (keysResizeWindow (12,12) (0.5,0.5)))
        , ((modm .|. controlMask, xK_l      ), withFocused (keysResizeWindow (6,0) (0,0)))
        , ((modm .|. controlMask, xK_h      ), withFocused (keysResizeWindow (-6,0) (0,0)))
        , ((modm .|. controlMask, xK_a      ), withFocused (keysResizeWindow (0,-6) (0,0)))
        , ((modm .|. controlMask, xK_z      ), withFocused (keysResizeWindow (0,6) (0,0)))
-       , ((modm                , xK_Right  ), nextWS ) -- go to next workspace
-       , ((modm                , xK_Left   ), prevWS ) -- go to prev workspace
+       -- Go to the next / previous workspace
+       , ((modm                , xK_Right  ), nextWS )
+       , ((modm                , xK_Left   ), prevWS )
+       -- Shift the focused window to the next / previous workspace
        , ((modm .|. shiftMask  , xK_Right  ), shiftToNext)
        , ((modm .|. shiftMask  , xK_Left   ), shiftToPrev)
-       , ((modm                , xK_comma  ), sendMessage Mag.MagnifyLess) -- smaller window
-       , ((modm                , xK_period ), sendMessage Mag.MagnifyMore) -- bigger window
+       -- Move the focus down / up
        , ((modm                , xK_j      ), windows W.focusDown)
        , ((modm                , xK_k      ), windows W.focusUp)
+       -- Swap the focused window down / up
        , ((modm .|. shiftMask  , xK_j      ), windows W.swapDown)
        , ((modm .|. shiftMask  , xK_k      ), windows W.swapUp)
+       -- Shift the focused window to the master window
        , ((modm .|. shiftMask  , xK_m      ), windows W.shiftMaster)
+       -- Search a window and focus into the window
        , ((modm                , xK_g      ), windowPromptGoto myXPConfig)
+       -- Search a window and bring to the current workspace
        , ((modm                , xK_b      ), windowPromptBring myXPConfig)
+       -- Move the focus to next screen (multi screen)
        , ((modm                , xK_w      ), nextScreen)
        ]
 
@@ -192,26 +197,40 @@ main = do
        ------------------------------------------------------------------------
 
        `additionalKeys`
-       [ ((mod1Mask .|. controlMask, xK_l      ), spawn "xscreensaver-command -lock")
+       [
+       -- Lock screen
+         ((mod1Mask .|. controlMask, xK_l      ), spawn "xscreensaver-command -lock")
+       -- Toggle compton (compsite manager)
        , ((mod1Mask .|. controlMask, xK_t      ), spawn "bash $HOME/bin/toggle_compton.sh")
+       -- Launch terminal
        , ((modm                    , xK_Return ), spawn "urxvtc")
+       -- Launch terminal with a float window
        , ((modm .|. shiftMask      , xK_Return ), spawn "sh $HOME/bin/urxvt_float.sh")
+       -- Insert a transparent panel
        , ((modm                    , xK_e      ), spawn "python $HOME/Workspace/python/transparent.py")
+       -- Launch dmenu for launching applicatiton
        , ((modm                    , xK_p      ), spawn "exe=`dmenu_run -b -p '#' -nb '#009688' -nf '#ffffff' -sb '#ffffff' -sf '#000000'` && exec $exe")
+       -- Lauch websearch application (See https://github.com/ssh0/web_search)
        , ((mod1Mask .|. controlMask, xK_f      ), spawn "python $HOME/Workspace/python/web_search/websearch.py")
-       , ((mod1Mask .|. controlMask, xK_s      ), spawn "python $HOME/Workspace/python/web_search/hlsearch.py")
+       -- Play / Pause media keys
        , ((0                       , 0x1008ff18), spawn "sh $HOME/bin/cplay.sh")
        , ((0                       , 0x1008ff14), spawn "sh $HOME/bin/cplay.sh")
+       -- Volume setting media keys
        , ((0                       , 0x1008ff13), spawn "bash $HOME/bin/sound_volume_change_wrapper.sh +")
        , ((0                       , 0x1008ff11), spawn "bash $HOME/bin/sound_volume_change_wrapper.sh -")
        , ((0                       , 0x1008ff12), spawn "bash $HOME/bin/sound_volume_change_wrapper.sh m")
         -- Brightness Keys
        , ((0                       , 0x1008FF02), spawn "xbacklight + 5 -time 100 -steps 1")
        , ((0                       , 0x1008FF03), spawn "xbacklight - 5 -time 100 -steps 1")
+       -- Take a screenshot (whole window)
        , ((0                       , 0xff61    ), spawn "sh $HOME/bin/screenshot.sh")
+       -- Take a screenshot (selected area)
        , ((shiftMask               , 0xff61    ), spawn "sh $HOME/bin/screenshot_select.sh")
+       -- Launch ipython qtconsole
        , ((0                       , 0x1008ff1d), spawn "ipython qtconsole --matplotlib=inline")
+       -- Toggle touchpad
        , ((controlMask             , xK_Escape ), spawn "bash $HOME/bin/touchpad_toggle.sh")
+       -- Toggle trackpoint (Lenovo PC)
        , ((mod1Mask                , xK_Escape ), spawn "bash $HOME/bin/trackpoint_toggle.sh")
        ]
 
@@ -224,7 +243,6 @@ myLayout = spacing gapWidth $ gaps [(U,gapWidth + gapWidthU),(D,gapWidth + gapWi
              ||| (TwoPane (1/100) (4/7))
              ||| (ThreeColMid 1 (1/100) (16/35))
              ||| (ResizableTall 2 (1/100) (1/2) [])
---             |||  Mag.magnifiercz 1.1 (spacing 6 $ GridRatio (4/3))
 
 --------------------------------------------------------------------------- }}}
 -- myStartupHook:     Start up applications                                 {{{
@@ -255,20 +273,19 @@ myManageHookShift = composeAll
 
 myManageHookFloat = composeAll
     [ className =? "Gimp"             --> doFloat
+    , className =? "Tk"               --> doFloat
     , className =? "mplayer2"         --> doCenterFloat
     , className =? "mpv"              --> doCenterFloat
-    , className =? "Tk"               --> doFloat
-    , className =? "Screenkey"        --> (doRectFloat $ W.RationalRect 0.7 0.9 0.3 0.1)
     , className =? "feh"              --> doCenterFloat
     , className =? "Display.im6"      --> doCenterFloat
     , className =? "Shutter"          --> doCenterFloat
     , className =? "Thunar"           --> doCenterFloat
     , className =? "Nautilus"         --> doCenterFloat
-    , className =? "Websearch.py"     --> (doRectFloat $ W.RationalRect 0.45 0.4 0.1 0.01)
-    , title     =? "urxvt_float"      --> doCenterFloat
     , className =? "Plugin-container" --> doCenterFloat
-
+    , className =? "Screenkey"        --> (doRectFloat $ W.RationalRect 0.7 0.9 0.3 0.1)
+    , className =? "Websearch.py"     --> (doRectFloat $ W.RationalRect 0.45 0.4 0.1 0.01)
     , title     =? "Speedbar"         --> doCenterFloat
+    , title     =? "urxvt_float"      --> doCenterFloat
     , stringProperty "WM_NAME" =? "LINE" --> (doRectFloat $ W.RationalRect 0.60 0.1 0.39 0.82)
     , stringProperty "WM_NAME" =? "Google Keep" --> (doRectFloat $ W.RationalRect 0.3 0.1 0.4 0.82)
     , stringProperty "WM_NAME" =? "tmptex.pdf - 1/1 (96 dpi)" --> (doRectFloat $ W.RationalRect 0.29 0.25 0.42 0.5)
