32777c2d959f3fc5136a985ab61f614509101384
Shotaro Fujimoto <fuji101ijuf@gmail.com>
2015-08-31 13:12:21 +0900

    modified downloads

--- luakit/rc.lua
+++ luakit/rc.lua
@@ -107,7 +107,7 @@ require "downloads"
 require "downloads_chrome"
 
 -- Set download location
-downloads.default_dir = "/media/shotaro/STOCK"
+downloads.default_dir = os.getenv("HOME") .. "/Downloads"
 downloads.add_signal("download-location", function (uri, file)
     if not file or file == "" then
         file = (string.match(uri, "/([^/]+)$")
