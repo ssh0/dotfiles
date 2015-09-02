4817be61c6a56fce26e7c6510c54bbac6165fd7c
Shotaro Fujimoto <fuji101ijuf@gmail.com>
2015-09-03 00:43:31 +0900

    :wrench: commentout add_signal("download-location")

--- luakit/rc.lua
+++ luakit/rc.lua
@@ -114,22 +114,22 @@ require "downloads"
 require "downloads_chrome"
 
 -- Set download location
-downloads.default_dir = os.getenv("HOME") .. "/Downloads"
-downloads.add_signal("download-location", function (uri, file)
-    if not file or file == "" then
-        file = (string.match(uri, "/([^/]+)$")
-            or string.match(uri, "^%w+://(.+)")
-            or string.gsub(uri, "/", "_")
-            or "untitled")
-    end
-    return downloads.default_dir .. "/" .. file
-end)
+-- downloads.default_dir = os.getenv("HOME") .. "/Downloads"
+-- downloads.add_signal("download-location", function (uri, file)
+--     if not file or file == "" then
+--         file = (string.match(uri, "/([^/]+)$")
+--             or string.match(uri, "^%w+://(.+)")
+--             or string.gsub(uri, "/", "_")
+--             or "untitled")
+--     end
+--     return downloads.default_dir .. "/" .. file
+-- end)
 
 -- Example using xdg-open for opening downloads / showing download folders
---downloads.add_signal("open-file", function (file, mime)
---    luakit.spawn(string.format("xdg-open %q", file))
---    return true
---end)
+downloads.add_signal("open-file", function (file, mime)
+    luakit.spawn(string.format("xdg-open %q", file))
+    return true
+end)
 
 -- Add vimperator-like link hinting & following
 require "follow"
