6407ed3c566f58a1ec15324de9a5c9acf0d59d41
Shotaro Fujimoto <fuji101ijuf@gmail.com>
2015-09-12 02:00:55 +0900

    modified

--- luakit/theme.lua
+++ luakit/theme.lua
@@ -5,65 +5,75 @@
 local theme = {}
 
 -- Default settings
-theme.font = "Migu 1M 10.5"
-theme.fg   = "#ffffff"
-theme.bg   = "#1c1c1c"
+theme.font   = "Migu 1M 10.5"
+
+theme.fg     = "#ffffff"
+theme.fg1    = "#c6c6c6"
+theme.bg     = "#1c1c1c"
+theme.black  = "#353535"
+theme.black2 = "#474747"
+theme.black3 = "#9e9e9e"
+theme.red    = "#ef9a9a"
+theme.green  = "#a5d6a7"
+theme.green2 = "#b9e18b"
+theme.yellow = "#ece391"
+theme.white  = "#aaaaaa"
 
 -- Genaral colours
-theme.success_fg = "#a5d6a7"
+theme.success_fg = theme.green
 theme.loaded_fg  = "#33aadd"
-theme.error_fg = "#1c1c1c"
-theme.error_bg = "#ef9a9a"
+theme.error_fg   = theme.bg
+theme.error_bg   = theme.red
 
 -- Warning colours
-theme.warning_fg = "#1c1c1c"
-theme.warning_bg = "#ef9a9a"
+theme.warning_fg = theme.bg
+theme.warning_bg = theme.red
 
 -- Notification colours
-theme.notif_fg = "#ffffff"
-theme.notif_bg = "#353535"
+theme.notif_fg = theme.fg
+theme.notif_bg = theme.black
 
 -- Menu colours
-theme.menu_fg                   = "#c6c6c6"
-theme.menu_bg                   = "#1c1c1c"
-theme.menu_selected_fg          = "#ffffff"
-theme.menu_selected_bg          = "#474747"
-theme.menu_title_bg             = "#141414"
-theme.menu_primary_title_fg     = "#b9e18b"
-theme.menu_secondary_title_fg   = "#b9e18b"
+theme.menu_fg                   = theme.fg1
+theme.menu_bg                   = theme.bg
+theme.menu_selected_fg          = theme.fg
+theme.menu_selected_bg          = theme.black2
+theme.menu_title_bg             = theme.bg
+theme.menu_primary_title_fg     = theme.green2
+theme.menu_secondary_title_fg   = theme.green2
 
 -- Proxy manager
-theme.proxy_active_menu_fg      = '#c6c6c6'
-theme.proxy_active_menu_bg      = '#1c1c1c'
-theme.proxy_inactive_menu_fg    = '#aaaaaa'
-theme.proxy_inactive_menu_bg    = '#1c1c1c'
+theme.proxy_active_menu_fg      = theme.fg1
+theme.proxy_active_menu_bg      = theme.bg
+theme.proxy_inactive_menu_fg    = theme.white
+theme.proxy_inactive_menu_bg    = theme.bg
 
 -- Statusbar specific
-theme.sbar_fg         = "#9e9e9e"
-theme.sbar_bg         = "#1c1c1c"
+theme.sbar_fg         = theme.black3
+theme.sbar_bg         = theme.bg
 
 -- Downloadbar specific
-theme.dbar_fg         = "#aaaaaa"
-theme.dbar_bg         = "#1c1c1c"
-theme.dbar_error_fg   = "#ef9a9a"
+theme.dbar_fg         = theme.white
+theme.dbar_bg         = theme.bg
+theme.dbar_error_fg   = theme.red
 
 -- Input bar specific
-theme.ibar_fg           = "#c6c6c6"
-theme.ibar_bg           = "#1c1c1c"
+theme.ibar_fg           = theme.fg1
+theme.ibar_bg           = theme.bg
 
 -- Tab label
-theme.tab_fg            = "#9e9e9e"
-theme.tab_bg            = "#1c1c1c"
-theme.tab_ntheme        = "#bdbdbd"
-theme.selected_fg       = "#ffffff"
-theme.selected_bg       = "#474747"
-theme.selected_ntheme   = "#bdbdbd"
-theme.loading_fg        = "#ffffff"
-theme.loading_bg        = "#363636"
+theme.tab_fg            = theme.black3
+theme.tab_bg            = theme.bg
+theme.tab_ntheme        = theme.yellow
+theme.selected_fg       = theme.fg
+theme.selected_bg       = theme.black2
+theme.selected_ntheme   = theme.yellow
+theme.loading_fg        = theme.fg
+theme.loading_bg        = theme.black
 
 -- Trusted/untrusted ssl colours
-theme.trust_fg          = "#a5d6a7"
-theme.notrust_fg        = "#ef9a9a"
+theme.trust_fg          = theme.green
+theme.notrust_fg        = theme.red
 
 return theme
 -- vim: et:sw=4:ts=8:sts=4:tw=80
