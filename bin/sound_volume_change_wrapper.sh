b1ac537c37eaa18a6e3d9d0d7757ea77d10c664f
ssh0 <fuji101ijuf@gmail.com>
2015-10-02 14:08:00 +0900

    rm volumeinfo saving process

--- bin/sound_volume_change_wrapper.sh
+++ bin/sound_volume_change_wrapper.sh
@@ -2,7 +2,7 @@
 # written by Shotaro Fujimoto (https://github.com/ssh0)
 # first edited: 2015-06-03
 
-volfifo="$HOME/.vol"
+# volfifo="$HOME/.vol"
 
 case "$1" in
   "+")
@@ -13,4 +13,4 @@ case "$1" in
     amixer -D pulse set Master toggle ;;
 esac
 
-amixer -D pulse get Master | awk -F'[]%[]' '/%/ {if ($7 == "off") { print "MM" } else { print $2 }}' | head -n 1 > "$volfifo"
+# amixer -D pulse get Master | awk -F'[]%[]' '/%/ {if ($7 == "off") { print "MM" } else { print $2 }}' | head -n 1 > "$volfifo"
