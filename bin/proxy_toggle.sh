206fd43cc5180f2c036009e84dd540429b7dff2b
ssh0 <fuji101ijuf@gmail.com>
2015-10-18 00:59:35 +0900

    proxy_toggle.sh: modified

--- bin/proxy_toggle.sh
+++ bin/proxy_toggle.sh
@@ -1,25 +1,22 @@
 #!/bin/sh
 
-HTTP_PROXY_HOST=www-proxy.waseda.jp
-HTTP_PROXY_PORT=8080
-HTTPS_PROXY_HOST=www-proxy.waseda.jp
-HTTPS_PROXY_PORT=8080
-FTP_PROXY_HOST=proxy.waseda.jp
-FTP_PROXY_PORT=21
+HTTP_PROXY_HOST="http://www-proxy.waseda.jp:8080"
+HTTPS_PROXY_HOST="https://www-proxy.waseda.jp:8080"
+FTP_PROXY_HOST="ftp://proxy.waseda.jp:21"
 
 shellrc=$HOME/.zshrc  # .bashrc
 
 if [ $# = 0 ]; then
-    echo "Error: no args"
-    exit 1
+  echo "Error: no args"
+  exit 1
 else
-    if [ $1 = "on" ]; then
+  if [ $1 = "on" ]; then
     sudo sed -i.bak '/http[s]::proxy/Id' /etc/apt/apt.conf
     sudo sed -i.bak '/ftp::proxy/Id' /etc/apt/apt.conf
     sudo tee -a /etc/apt/apt.conf <<EOF
-Acquire::ftp::proxy "ftp://$HTTP_PROXY_HOST:$HTTP_PROXY_PORT/";
-Acquire::http::proxy "http://$HTTP_PROXY_HOST:$HTTP_PROXY_PORT/";
-Acquire::https::proxy "https://$HTTPS_PROXY_HOST:$HTTPS_PROXY_PORT/";
+    Acquire::http::proxy "$HTTP_PROXY_HOST/";
+    Acquire::https::proxy "$HTTPS_PROXY_HOST/";
+    Acquire::ftp::proxy "$FTP_PROXY_HOST/";
 EOF
 
     sudo sed -i.bak "/http_proxy/Id" /etc/environment
@@ -29,12 +26,12 @@ EOF
     sudo sed -i.bak "/ftp_proxy/Id" /etc/environment
     sudo sed -i.bak "/FTP_PROXY/Id" /etc/environment
     sudo tee -a /etc/environment <<EOF
-http_proxy=http://$HTTP_PROXY_HOST:$HTTP_PROXY_PORT
-HTTP_PROXY=http://$HTTP_PROXY_HOST:$HTTP_PROXY_PORT
-https_proxy=http://$HTTPS_PROXY_HOST:$HTTPS_PROXY_PORT
-HTTPS_PROXY=http://$HTTPS_PROXY_HOST:$HTTPS_PROXY_PORT
-ftp_proxy=ftp://$HTTP_PROXY_HOST:$HTTP_PROXY_PORT
-FTP_PROXY=ftp://$HTTP_PROXY_HOST:$HTTP_PROXY_PORT
+    http_proxy=$HTTP_PROXY_HOST
+    HTTP_PROXY=$HTTP_PROXY_HOST
+    https_proxy=$HTTPS_PROXY_HOST
+    HTTPS_PROXY=$HTTPS_PROXY_HOST
+    ftp_proxy=$FTP_PROXY_HOST
+    FTP_PROXY=$FTP_PROXY_HOST
 EOF
     sed -i.bak '/http_proxy/Id' $shellrc
     sed -i.bak '/HTTP_PROXY/Id' $shellrc
@@ -43,42 +40,42 @@ EOF
     sed -i.bak '/ftp_proxy/Id' $shellrc
     sed -i.bak '/FTP_PROXY/Id' $shellrc
     tee -a $shellrc <<EOF
-export http_proxy=http://$HTTP_PROXY_HOST:$HTTP_PROXY_PORT
-export HTTP_PROXY=http://$HTTP_PROXY_HOST:$HTTP_PROXY_PORT
-export https_proxy=http://$HTTPS_PROXY_HOST:$HTTPS_PROXY_PORT
-export HTTPS_PROXY=http://$HTTPS_PROXY_HOST:$HTTPS_PROXY_PORT
-export ftp_proxy=ftp://$HTTP_PROXY_HOST:$HTTP_PROXY_PORT
-export FTP_PROXY=ftp://$HTTP_PROXY_HOST:$HTTP_PROXY_PORT
+    export http_proxy=$HTTP_PROXY_HOST
+    export HTTP_PROXY=$HTTP_PROXY_HOST
+    export https_proxy=$HTTPS_PROXY_HOST
+    export HTTPS_PROXY=$HTTPS_PROXY_HOST
+    export ftp_proxy=$FTP_PROXY_HOST
+    export FTP_PROXY=$FTP_PROXY_HOST
+EOF
+  else
+    if [ $1 = "off" ]; then
+      sudo sed -i.bak "/http::proxy/Id" /etc/apt/apt.conf
+      sudo sed -i.bak "/https::proxy/Id" /etc/apt/apt.conf
+      sudo sed -i.bak "/ftp::proxy/Id" /etc/apt/apt.conf
+      sudo sed -i.bak "/http_proxy/Id" /etc/environment
+      sudo sed -i.bak "/HTTP_PROXY/Id" /etc/environment
+      sudo sed -i.bak "/https_proxy/Id" /etc/environment
+      sudo sed -i.bak "/HTTPS_PROXY/Id" /etc/environment
+      sudo sed -i.bak "/ftp_proxy/Id" /etc/environment
+      sudo sed -i.bak "/FTP_PROXY/Id" /etc/environment
+      sed -i.bak '/http_proxy/Id' $shellrc
+      sed -i.bak '/HTTP_PROXY/Id' $shellrc
+      sed -i.bak '/https_proxy/Id' $shellrc
+      sed -i.bak '/HTTPS_PROXY/Id' $shellrc
+      sed -i.bak '/ftp_proxy/Id' $shellrc
+      sed -i.bak '/FTP_PROXY/Id' $shellrc
+      tee -a $shellrc <<EOF
+      export http_proxy=
+      export HTTP_PROXY=
+      export https_proxy=
+      export HTTPS_PROXY=
+      export ftp_proxy=
+      export FTP_PROXY=
 EOF
     else
-        if [ $1 = "off" ]; then
-            sudo sed -i.bak "/http::proxy/Id" /etc/apt/apt.conf
-            sudo sed -i.bak "/https::proxy/Id" /etc/apt/apt.conf
-            sudo sed -i.bak "/ftp::proxy/Id" /etc/apt/apt.conf
-            sudo sed -i.bak "/http_proxy/Id" /etc/environment
-            sudo sed -i.bak "/HTTP_PROXY/Id" /etc/environment
-            sudo sed -i.bak "/https_proxy/Id" /etc/environment
-            sudo sed -i.bak "/HTTPS_PROXY/Id" /etc/environment
-            sudo sed -i.bak "/ftp_proxy/Id" /etc/environment
-            sudo sed -i.bak "/FTP_PROXY/Id" /etc/environment
-            sed -i.bak '/http_proxy/Id' $shellrc
-            sed -i.bak '/HTTP_PROXY/Id' $shellrc
-            sed -i.bak '/https_proxy/Id' $shellrc
-            sed -i.bak '/HTTPS_PROXY/Id' $shellrc
-            sed -i.bak '/ftp_proxy/Id' $shellrc
-            sed -i.bak '/FTP_PROXY/Id' $shellrc
-            tee -a $shellrc <<EOF
-export http_proxy=
-export HTTP_PROXY=
-export https_proxy=
-export HTTPS_PROXY=
-export ftp_proxy=
-export FTP_PROXY=
-EOF
-        else
-            echo "arg: 'on' or 'off'"
-            exit 1
-        fi
+      echo "arg: 'on' or 'off'"
+      exit 1
     fi
+  fi
 fi
 exit 0
