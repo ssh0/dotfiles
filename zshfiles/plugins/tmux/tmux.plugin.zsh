0639a75439c97732c9e40e69d4f552e8c69cd6a1
ssh0 <fuji101ijuf@gmail.com>
2015-11-10 23:22:19 +0900

    tmux.plugins.zsh: don't use tmux aliases

--- zshfiles/plugins/tmux/tmux.plugin.zsh
+++ zshfiles/plugins/tmux/tmux.plugin.zsh
@@ -2,11 +2,11 @@
 # Aliases
 #
 
-alias ta='tmux attach -t'
-alias ts='tmux new-session -s'
-alias tl='tmux list-sessions'
-alias tksv='tmux kill-server'
-alias tkss='tmux kill-session -t'
+# alias ta='tmux attach -t'
+# alias ts='tmux new-session -s'
+# alias tl='tmux list-sessions'
+# alias tksv='tmux kill-server'
+# alias tkss='tmux kill-session -t'
 
 # Only run if tmux is actually installed
 if which tmux &> /dev/null
