6a0386f2108bba7b49536ca64d7ec295893c7e2a
ssh0 <fuji101ijuf@gmail.com>
2015-11-27 22:33:41 +0900

    aliases.zsh: Move "export" to zshrc, rm takenote modify shtest

--- zshfiles/aliases.zsh
+++ zshfiles/aliases.zsh
@@ -1,25 +1,6 @@
 # vim: set ft=zsh
 # written by Shotaro Fujimoto (https://github.com/ssh0)
 #------------------------------------------------------------------------------
-# export variables                                                          {{{
-#------------------------------------------------------------------------------
-
-# find alternative apps if it is installed on your system
-find_alt() { for i;do hash "$i" >/dev/null && { echo "$i"; return 0;};done;return 1; }
-
-# set the default program
-# the first program in the array will be chosen as the default
-export OPENER=$(find_alt xdg-open exo-open gnome-open )
-export BROWSER=$(find_alt luakit firefox google-chrome chromium chromium-browser $OPENER )
-export BROWSERCLI=$(find_alt w3m links2 links lynx elinks $OPENER )
-export EDITOR=$(find_alt vim emacs nano leafpad gedit pluma $OPENER )
-export FILEMANAGER=$(find_alt thunar nautilus dolphin pcmanfm tspacefm enlightenment_filemanager $OPENER )
-export PAGER=$(find_alt less more most)
-export PLAYER=$(find_alt mpv mplayer cvlc $OPENER )
-export READER=$(find_alt mupdf zathura evince $OPENER )
-export TERMCMD=$(find_alt urxvt xterm gnome-terminal)
-
-#---------------------------------------------------------------------------}}}
 # Alias                                                                     {{{
 #------------------------------------------------------------------------------
 
@@ -172,233 +153,41 @@ function agvim(){
 }
 
 #---------------------------------------------------------------------------}}}
-# takenote                                                                  {{{
-#------------------------------------------------------------------------------
-
-function takenote() {
-  local get_dir=false
-  local get_name=false
-  local alternative=false
-  local show_list=false
-  local filercmd=r
-  local filer=false
-  local rootdir=$HOME/Workspace/blog
-  local pattern='s/note_\([0-9]\{2\}\).md/\1/p'
-
-  show_usage() {
-    echo "Usage: takenote [-d dir] [-o filename] [-g editor] [-l] [-r] [-h]"
-    echo "  -d dir     : set the saving directory (default: '$rootdir')"
-    echo "  -o filename: set the file's name"
-    echo "  -g editor  : open with an altenative program (default: '$EDITOR')"
-    echo "  -l         : list the today's files"
-    echo "  -r         : open the today's dir or the root dir with the filer (default: '$filercmd')"
-    echo "  -h         : show this message"
-  }
-
-  check_dir() {
-    if [ ! -e "$1" ]; then
-      echo "Directory '$1' doesn't exist."
-      return 1
-    fi
-  }
-
-  while getopts d:o:g:lrh OPT
-  do
-    case $OPT in
-      "d" ) get_dir=true
-            dir="$OPTARG" ;;
-      "o" ) get_name=true
-            name="$OPTARG" ;;
-      "g" ) alternative=true
-            editor="$OPTARG" ;;
-      "l" ) show_list=true ;;
-      "r" ) filer=true ;;
-      "h" ) show_usage;
-            return 0 ;;
-        * ) show_usage;
-            return 0 ;;
-    esac
-  done
-
-  # set the saving directory
-  if ! $get_dir; then
-    if check_dir "$rootdir"; then
-      daydir=$(date +%Y-%m-%d)
-      dir=$rootdir/$daydir
-    else
-      return 1
-    fi
-  fi
-
-  # only show existing file in the directory
-  if $show_list; then
-    if check_dir "$dir"; then
-      list=$(ls "$dir")
-      echo "$list"
-    fi
-    return 0
-  fi
-
-  # move to today's directory by the user defined filer
-  if $filer; then
-    if [ ! -e "$dir" ]; then
-      echo "Today's directory has not been created yet."
-      echo "Open root directory '$rootdir' ..."
-      "$filercmd" "$rootdir"
-    else
-      "$filercmd" "$dir"
-    fi
-    return 0
-  fi
-
-  # set the filename
-  if ! $get_name; then
-    if [ ! -e "$dir" ]; then
-      i=1
-    else
-      i=$(( $(ls "$dir" | sed -n $pattern | tail -n 1) + 1 ))
-    fi
-    name="$(printf note_%02d.md "$i")"
-  fi
-
-  # change directory
-  local cwd="`pwd`"
-  cd $dir
-
-  if $alternative; then
-    $editor "$dir/$name"
-  else
-    $EDITOR "$dir/$name"
-  fi
-
-  # back to recent working directory
-  cd $cwd
-
-  return 0
-}
-
-#---------------------------------------------------------------------------}}}
 # shtest                                                                    {{{
 #------------------------------------------------------------------------------
 
 function shtest() {
+
   show_usage() {
     echo ""
-    echo "Make testing snippet for shell script."
+    echo "shtest - Make testing snippet for shell script."
     echo ""
-    echo "Usage: show_usage [OPTION]"
+    echo "Usage: shtest [OPTION]"
     echo ""
     echo "Option:"
     echo "  -o FILE: Set the file name to save. "
-    echo "  -l     : List the formatted files."
+    echo "  -l     : List the files."
     echo "  -h     : Show this message."
   }
 
-  local pattern='s/test_\([0-9]\{2\}\).sh/\1/p'
-  local pattern_full='s/\(test_[0-9]\{2\}.sh\)/\1/p'
-  local get_name=false
-  local dir="$HOME/bin"
-  local cwd="`pwd`"
-
-  show_list() {
-    list="$(ls "$dir" | sed -n $pattern_full)"
-    echo "$list"
-  }
-
-  while getopts o:lh OPT
-  do
-    case $OPT in
-      "o" ) get_name=true
-            name="$OPTARG" ;;
-      "l" ) show_list
-            return 0 ;;
-      "h" ) show_usage;
-            return 1 ;;
-        * ) show_usage;
-            return 1 ;;
-    esac
-  done
-
-  if ! $get_name; then
-    i=$(( $(ls "$dir" | sed -n $pattern | tail -n 1) + 1 ))
-    name="$(printf test_%02d.sh "$i")"
+  if [ "$1" = "-h" ]; then
+    show_usage
+    return 1
   fi
 
-  # change directory
-  cd $dir
-  $EDITOR "$dir/$name"
+  local _TAKENOTE_FILENAME_PRE="${TAKENOTE_FILENAME_PRE}"
+  local _TAKENOTE_FILENAME_EXTENSION="${TAKENOTE_FILENAME_EXTENSION}"
 
-  # return to recent working directory
-  cd $cwd
+  TAKENOTE_FILENAME_PRE="test_"
+  TAKENOTE_FILENAME_EXTENSION="sh"
+
+  takenote -d "$HOME/bin" $@
+
+  export TAKENOTE_FILENAME_PRE="${_TAKENOTE_FILENAME_PRE}"
+  export TAKENOTE_FILENAME_EXTENSION="${_TAKENOTE_FILENAME_EXTENSION}"
 
   return 0
 }
-
-#---------------------------------------------------------------------------}}}
-# zsh-bd                                                                    {{{
-#------------------------------------------------------------------------------
-# Quickly go back to aspecific parent directory instead of typing cd ../../../
-# [Tarrasch/zsh-bd](https://github.com/Tarrasch/zsh-bd)
-
-function bd () {
-  (($#<1)) && {
-    print -- "usage: $0 <name-of-any-parent-directory>"
-    print -- "       $0 <number-of-folders>"
-    return 1
-  } >&2
-  local num=${#${(ps:/:)${PWD} }}
-  local dest="./"
-
-  # If the user provided an integer, go up as many times as asked
-  if [[ "$1" = <-> ]]
-  then
-    if [[ $1 -gt $num ]]
-    then
-      print -- "bd: Error: Can not go up $1 times (not enough parent directories)"
-      return 1
-    fi
-    for i in {1..$1}
-    do
-      dest+="../"
-    done
-    cd $dest
-    return 0
-  fi
-
-  # else, find the correct parent directory
-  # Get parents (in reverse order)
-  local parents
-  local i
-  for i in {$((num+1))..2}
-  do
-    parents=($parents "$(echo $PWD | cut -d'/' -f$i)")
-  done
-  parents=($parents "/")
-  # Build dest and 'cd' to it
-  local parent
-  foreach parent (${parents})
-  do
-    if [[ $1 == $parent ]]
-    then
-      cd $dest
-      return 0
-    fi
-    dest+="../"
-  done
-  print -- "bd: Error: No parent directory named '$1'"
-  return 1
-}
-_bd () {
-  # Get parents (in reverse order)
-  local num=${#${(ps:/:)${PWD}} }
-  local i
-  for i in {$((num+1))..2}
-  do
-    reply=($reply "`echo $PWD | cut -d'/' -f$i`")
-  done
-  reply=($reply "/")
-}
-compctl -V directories -K _bd bd
 
 #---------------------------------------------------------------------------}}}
 # Notification of local host command {{{
@@ -513,11 +302,11 @@ fi
 # Configurations                                                            {{{
 #------------------------------------------------------------------------------
 
-cfg-aliases() { $EDITOR $ZSH/aliases.zsh ;}
+cfg-aliases() { $EDITOR ${ZSH_ROOT}/aliases.zsh ;}
 cfg-compton() { $EDITOR ~/.config/compton/compton.conf ;}
 cfg-dotlink() { $EDITOR ~/.dotfiles/dotlink ;}
 cfg-dotrc() { $EDITOR ~/.dotfiles/dotrc ;}
-cfg-history() { $EDITOR $ZSH/history ;}
+cfg-history() { $EDITOR ${ZSH_ROOT}/history ;}
 cfg-latexmkrc() { $EDITOR ~/.latexmkrc ;}
 cfg-luakit() { $EDITOR ~/.config/luakit ;}
 cfg-mpv() { $EDITOR ~/.mpv/config ;}
