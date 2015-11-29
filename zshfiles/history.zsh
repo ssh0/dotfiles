a6b7ccd1905fd65279c1e593fa6a59e8b85020ee
Shotaro Fujimoto <fuji101ijuf@gmail.com>
2015-11-30 04:05:05 +0900

    Divide history parts to other file (history.zsh)

---
+++ zshfiles/history.zsh
@@ -0,0 +1,54 @@
+unsetopt flowcontrol
+setopt auto_menu         # show completion menu on succesive tab press
+setopt interactive_comments
+setopt complete_in_word
+setopt always_to_end
+setopt auto_param_slash
+
+export WORDCHARS=''
+
+zstyle ':completion:*' verbose yes
+zstyle ':completion:*' format '%B%F{magenta}%d%b%f'
+
+zstyle ':completion:*:default' menu select=2
+zstyle ':completion:*:messages' format '%F{yellow}%d'"$DEFAULT"
+zstyle ':completion:*:warnings' format '%F{red}No matches for:''%F{yelllow} %d'"$DEFAULT"
+zstyle ':completion:*:options' description 'yes'
+zstyle ':completion:*:descriptions' format '%F{yellow}Completing %B%d%b%f'"$DEFAULT"
+
+zstyle ':completion:*' group-name ''
+
+# case sensitive (but if upper case, don't translate it to lower case.)
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z-_}={A-Za-z_-}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+
+zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
+
+zstyle ':completion:*:*:*:*:*' menu select
+zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
+zstyle ':completion:*:*:*:*:processes' command "ps -u $USER -o pid,user,comm -w -w"
+
+# disable named-directories autocompletion
+zstyle ':completion:*:cd:*' tag-order local-directories directory-stack path-directories
+
+# Use caching so that commands like apt and dpkg complete are useable
+zstyle ':completion::complete:*' use-cache true
+zstyle ':completion::complete:*' cache-path "$ZSH_CACHE_DIR"
+
+# Don't complete uninteresting users
+zstyle ':completion:*:*:*:users' ignored-patterns \
+        adm amanda apache at avahi avahi-autoipd beaglidx bin cacti canna \
+        clamav daemon dbus distcache dnsmasq dovecot fax ftp games gdm \
+        gkrellmd gopher hacluster haldaemon halt hsqldb ident junkbust kdm \
+        ldap lp mail mailman mailnull man messagebus  mldonkey mysql nagios \
+        named netdump news nfsnobody nobody nscd ntp nut nx obsrun openvpn \
+        operator pcap polkitd postfix postgres privoxy pulse pvm quagga radvd \
+        rpc rpcuser rpm rtkit scard shutdown squid sshd statd svn sync tftp \
+        usbmux uucp vcsa wwwrun xfs '_*'
+
+# ... unless we really want to.
+zstyle '*' single-ignored show
+
+# history search
+autoload history-search-end
+zle -N history-beginning-search-backward-end history-search-end
+zle -N history-beginning-search-forward-end history-search-end
