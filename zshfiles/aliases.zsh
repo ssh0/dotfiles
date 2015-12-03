b95e8129d24c140082f2ca2bfa3d652099708418
Shotaro Fujimoto <fuji101ijuf@gmail.com>
2015-12-03 22:17:26 +0900

    shtest: Add completion

--- zshfiles/aliases.zsh
+++ zshfiles/aliases.zsh
@@ -160,6 +160,10 @@ function agvim(){
 # shtest                                                                    {{{
 #------------------------------------------------------------------------------
 
+SHTEST_FILENAME_PRE="test_"
+SHTEST_FILENAME_EXTENSION="sh"
+
+
 function shtest() {
 
   show_usage() {
@@ -182,8 +186,8 @@ function shtest() {
   local _TAKENOTE_FILENAME_PRE="${TAKENOTE_FILENAME_PRE}"
   local _TAKENOTE_FILENAME_EXTENSION="${TAKENOTE_FILENAME_EXTENSION}"
 
-  TAKENOTE_FILENAME_PRE="test_"
-  TAKENOTE_FILENAME_EXTENSION="sh"
+  TAKENOTE_FILENAME_PRE="${SHTEST_FILENAME_PRE}"
+  TAKENOTE_FILENAME_EXTENSION="${SHTEST_FILENAME_EXTENSION}"
 
   takenote -d "$HOME/bin" $@
 
@@ -192,6 +196,19 @@ function shtest() {
 
   return 0
 }
+
+_shtest() {
+  local pattern='s/\('"${SHTEST_FILENAME_PRE}"'[0-9]\{'"${TAKENOTE_FILENAME_NUMORDER}"'\}'"${TAKENOTE_FILENAME_POST}"'.'"${SHTEST_FILENAME_EXTENSION}"'\)/\1/p'
+  typeset -A opt_args
+  _arguments -S \
+    "(-l -h)-o[Set the text file's name.]:files:( `shtest -l | sed -n $pattern ` )" \
+    "(-o -h)-l[Show the files in the today's directory.]: :" \
+    "(-o -l)-h[Show the help message.]: :" \
+    && return 0
+}
+
+compdef _shtest shtest
+
 
 #---------------------------------------------------------------------------}}}
 # Notification of local host command {{{
