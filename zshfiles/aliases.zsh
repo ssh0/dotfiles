a40486499d49fc14f6c03a8936cff8e42ac5155a
Shotaro Fujimoto <fuji101ijuf@gmail.com>
2015-12-06 20:08:56 +0900

    aliases.zsh: add function 'man' available for buitlin command

--- zshfiles/aliases.zsh
+++ zshfiles/aliases.zsh
@@ -95,6 +95,27 @@ alias -s {qz,tgz,zip,lzh,bz2,tbz,Z,tar,arj,xz}=extract
 alias -s {png,jpg,bmp,PNG,JPG,BMP}='feh --scale-down'
 
 #---------------------------------------------------------------------------}}}
+# man (available for buitlin)                                               {{{
+#------------------------------------------------------------------------------
+# Can I get individual man pages for the bash builtin commands? - Unix & Linux Stack Exchange
+#   http://unix.stackexchange.com/questions/18087/can-i-get-individual-man-pages-for-the-bash-builtin-commands
+# manpage - How to make `man` work for shell builtin commands and keywords? - Ask Ubuntu
+#   http://askubuntu.com/questions/439410/how-to-make-man-work-for-shell-builtin-commands-and-keywords
+
+function man() {
+  case "$(bash -c "type -t -- $1"):$1" in
+    builtin:*) # built-in
+      bash -c "help -m $1 | ${PAGER:-less}"
+      ;;
+    *[[?*]*) # pattern
+      bash -c "help -m $1 | ${PAGER:-less}"
+      ;;
+    *) command -p man "$@";;  # something else, presumed to be an external command
+                              # or options for the man command or a section number
+  esac
+}
+
+#---------------------------------------------------------------------------}}}
 # ranger-cd                                                                 {{{
 #------------------------------------------------------------------------------
 # Compatible with ranger 1.4.2 through 1.7.*
