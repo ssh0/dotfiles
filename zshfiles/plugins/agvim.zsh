56876146268e6dbad89137fcb149e8bc2bcd2f48
Shotaro Fujimoto <fuji101ijuf@gmail.com>
2015-12-09 02:06:39 +0900

    agvim.zsh: Refatoring (shell like)

--- zshfiles/plugins/agvim.zsh
+++ zshfiles/plugins/agvim.zsh
@@ -7,7 +7,7 @@
 function agvim() {
   local agfilepath
   agfilepath="$(echo $(ag $@ | peco --query "$LBUFFER" | awk -F : '{print "+" $2 " \047" $1 "\047"}'))"
-  if [ "$agfilepath" != "" ]; then
+  if [ -n "$agfilepath" ]; then
     eval $(echo "vim $agfilepath")
   fi
 }
