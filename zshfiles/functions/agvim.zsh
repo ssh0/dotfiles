459931979f70d4b9fd5e062e15c961f38945d598
Shotaro Fujimoto <fuji101ijuf@gmail.com>
2015-12-13 10:10:00 +0900

    agvim: Use PERCOL

--- zshfiles/functions/agvim.zsh
+++ zshfiles/functions/agvim.zsh
@@ -6,7 +6,7 @@
 
 function agvim() {
   local agfilepath
-  agfilepath="$(echo $(ag $@ | peco --query "$LBUFFER" | awk -F : '{print "+" $2 " \047" $1 "\047"}'))"
+  agfilepath="$(echo $(ag $@ | $PERCOL | awk -F : '{print "+" $2 " \047" $1 "\047"}'))"
   if [ -n "$agfilepath" ]; then
     eval $(echo "vim $agfilepath")
   fi
