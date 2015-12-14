81d07eb4e47cab9b54c0240e643e21df658ed3ce
Shotaro Fujimoto <fuji101ijuf@gmail.com>
2015-12-14 17:15:22 +0900

    cfg-alias.zsh: Add cfg-nvim (& :sort)

--- zshfiles/functions/cfg-alias.zsh
+++ zshfiles/functions/cfg-alias.zsh
@@ -10,22 +10,22 @@ cfg-functions() { $EDITOR ~/.zsh/functions ;}
 cfg-history() { $EDITOR ${ZSH_ROOT}/history ;}
 cfg-latexmkrc() { $EDITOR ~/.latexmkrc ;}
 cfg-luakit() { $EDITOR ~/.config/luakit ;}
-cfg-mpv() { $EDITOR ~/.mpv/config ;}
 cfg-mplayer() { $EDITOR ~/.mplayer/config ;}
+cfg-mpv() { $EDITOR ~/.mpv/config ;}
+cfg-nvim() { $EDITOR ~/.config/nvim/init.vim ;}
 cfg-ranger() { $EDITOR ~/.config/ranger/rc.conf ;}
-cfg-ranger-rifle() { $EDITOR ~/.config/ranger/rifle.conf ;} # edit open_with extensions
 cfg-ranger-commands() { $EDITOR ~/.config/ranger/commands.py ;} # scripts
+cfg-ranger-rifle() { $EDITOR ~/.config/ranger/rifle.conf ;} # edit open_with extensions
 cfg-tig() { $EDITOR ~/.tigrc ;}
 cfg-tmux() { $EDITOR ~/.tmux.conf ;}
 cfg-tmuxinator() { $EDITOR ~/.tmuxinator/ ;}
 cfg-turses() { $EDITOR ~/.turses/config ;}
+cfg-vim() { $EDITOR ~/.vimrc ;}
 cfg-vimcolor() { $EDITOR ~/.vim/bundle/easy-reading.vim/colors/easy-reading.vim ;}
 cfg-vimperatorrc() { $EDITOR ~/.vimperatorrc ;}
-cfg-vim() { $EDITOR ~/.vimrc ;}
+cfg-websearch() { $EDITOR ~/Workspace/python/web_search/websearch/config.py ;}
 cfg-xdefaults() { $EDITOR ~/.Xdefaults ;}
 cfg-xmodmap() { $EDITOR ~/.Xmodmap ;}
 cfg-xmonad() { $EDITOR ~/.xmonad/xmonad.hs ;}
 cfg-xresources() { $EDITOR ~/.Xresources ;}
-cfg-websearch() { $EDITOR ~/Workspace/python/web_search/websearch/config.py ;}
 cfg-zshrc() { $EDITOR ~/.zshrc ;}
-
