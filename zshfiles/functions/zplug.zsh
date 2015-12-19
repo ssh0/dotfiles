e1e2ec5ada33fc4874fb83d082f78e7f0ab9b186
Shotaro Fujimoto <fuji101ijuf@gmail.com>
2015-12-19 12:27:36 +0900

    zplug: Add initialise part

--- zshfiles/functions/zplug.zsh
+++ zshfiles/functions/zplug.zsh
@@ -1,31 +1,55 @@
 # zplug load settings
 
-source "$HOME/.zplug/zplug"
+zplug_source="$HOME/.zplug/repos/b4b4r07/zplug/zplug"
 
-zplug "lib/git", from:oh-my-zsh
-zplug "lib/grep", from:oh-my-zsh
-zplug "lib/history", from:oh-my-zsh
-zplug "lib/spectrum", from:oh-my-zsh
-zplug "lib/termsupport", from:oh-my-zsh
-zplug "lib/theme-and-appearance", from:oh-my-zsh
-
-zplug "zsh-users/zsh-syntax-highlighting"
-zplug "fcambus/ansiweather", as:command
-zplug "b4b4r07/enhancd"
-zplug "b4b4r07/zplug"
-zplug "Tarrasch/zsh-bd"
-zplug "chrissicool/zsh-256color"
-zplug "ssh0/zsh-takenote"
-zplug "ssh0/dot"
-
-# Install plugins if there are plugins that have not been installed
-if ! zplug check --verbose; then
-    printf "Install? [y/N]: "
-    if read -q; then
-        echo; zplug install
-    fi
+# if zplug is not installed, automatically clone it and reload shell
+if [[ ! -f "${zplug_source}" ]]; then
+  echo "zplug is not installed in this machine."
+  echo "Installing zplug..."
+  echo ""
+  curl -fLo "$HOME/.zplug/zplug" --create-dirs https://git.io/zplug
+  source "$HOME/.zplug/zplug"
+elif [[ -f $HOME/.zplug/zplug ]]; then
+  source "$HOME/.zplug/zplug"
+  rm "$HOME/.zplug/zplug"
 fi
 
-# Then, source plugins and add commands to $PATH
-# zplug load --verbose
-zplug load
+source ${zplug_source}
+unset -v zplug_source
+
+function zplug_reset() {
+  echo "zplug: Deleting $ZPLUG_EXTERNAL"
+  rm "${ZPLUG_EXTERNAL}"
+}
+
+if [[ -f $ZPLUG_EXTERNAL ]]; then
+  source $ZPLUG_EXTERNAL
+else
+  zplug "lib/git", from:oh-my-zsh
+  zplug "lib/grep", from:oh-my-zsh
+  zplug "lib/history", from:oh-my-zsh
+  zplug "lib/spectrum", from:oh-my-zsh
+  zplug "lib/termsupport", from:oh-my-zsh
+  zplug "lib/theme-and-appearance", from:oh-my-zsh
+
+  zplug "fcambus/ansiweather", as:command
+  zplug "b4b4r07/enhancd"
+  zplug "b4b4r07/zplug"
+  zplug "Tarrasch/zsh-bd"
+  zplug "chrissicool/zsh-256color"
+  zplug "ssh0/zsh-takenote"
+  zplug "ssh0/dot"
+  zplug "zsh-users/zsh-syntax-highlighting", nice:10
+
+  # Install plugins if there are plugins that have not been installed
+  if ! zplug check --verbose; then
+      printf "Install? [y/N]: "
+      if read -q; then
+          echo; zplug install
+      fi
+  fi
+
+  # Then, source plugins and add commands to $PATH
+  # zplug load --verbose
+  zplug load --verbose
+fi
