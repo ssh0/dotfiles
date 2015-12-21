065b034ec2d3173ed4312518e850d55599e22ee8
Shotaro Fujimoto <fuji101ijuf@gmail.com>
2015-12-22 04:28:24 +0900

    :fire:

--- zshfiles/functions/cfg-alias.zsh
+++ zshfiles/functions/cfg-alias.zsh
@@ -3,27 +3,3 @@
 # Configurations
 #------------------------------------------------------------------------------
 
-cf-compton() { $EDITOR ~/.config/compton/compton.conf ;}
-cf-dotlink() { $EDITOR ~/.dotfiles/dotlink ;}
-cf-dotrc() { $EDITOR ~/.dotfiles/dotrc ;}
-cf-functions() { $EDITOR ~/.zsh/functions ;}
-cf-history() { $EDITOR ${ZSH_ROOT}/history ;}
-cf-latexmkrc() { $EDITOR ~/.latexmkrc ;}
-cf-luakit() { $EDITOR ~/.config/luakit ;}
-cf-mplayer() { $EDITOR ~/.mplayer/config ;}
-cf-mpv() { $EDITOR ~/.mpv/config ;}
-cf-nvim() { $EDITOR ~/.config/nvim/init.vim ;}
-cf-ranger() { $EDITOR ~/.config/ranger/rc.conf ;}
-cf-ranger-rifle() { $EDITOR ~/.config/ranger/rifle.conf ;} # edit open_with extensions
-cf-tig() { $EDITOR ~/.tigrc ;}
-cf-tmux() { $EDITOR ~/.tmux.conf ;}
-cf-turses() { $EDITOR ~/.turses/config ;}
-cf-vim() { $EDITOR ~/.vimrc ;}
-cf-vimcolor() { $EDITOR ~/.vim/bundle/easy-reading.vim/colors/easy-reading.vim ;}
-cf-vimperatorrc() { $EDITOR ~/.vimperatorrc ;}
-cf-websearch() { $EDITOR ~/Workspace/python/web_search/websearch/config.py ;}
-cf-xdefaults() { $EDITOR ~/.Xdefaults ;}
-cf-xmodmap() { $EDITOR ~/.Xmodmap ;}
-cf-xmonad() { $EDITOR ~/.xmonad/xmonad.hs ;}
-cf-xresources() { $EDITOR ~/.Xresources ;}
-cf-zshrc() { $EDITOR ~/.zshrc ;}
