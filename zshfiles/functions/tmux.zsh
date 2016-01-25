0d840b2feab62bbc9c117e5f0d3ed4a9b71e388a
Shotaro Fujimoto <fuji101ijuf@gmail.com>
2016-01-25 22:37:27 +0900

    tmux.zsh: Add feature - automatically start tmux

--- zshfiles/functions/tmux.zsh
+++ zshfiles/functions/tmux.zsh
@@ -7,3 +7,26 @@ alias tl='tmux list-sessions'
 alias tksv='tmux kill-server'
 alias tkss='tmux kill-session -t'
 
+# Only run if tmux is actually installed
+if hash tmux; then
+  # Wrapper function for tmux.
+  function _zsh_tmux_plugin_run() {
+    # We have other arguments, just run them
+    if [[ -n "$@" ]]; then
+      \tmux $@
+      # Just run tmux, fixing the TERM variable if requested.
+    else
+      \tmux && exit 0
+    fi
+  }
+
+  # Use the completions for tmux for our function
+  compdef _tmux _zsh_tmux_plugin_run
+
+  # Alias tmux to our wrapper function.
+  alias tmux=_zsh_tmux_plugin_run
+
+  # Autostart if not already in tmux.
+  [[ ! -n "$TMUX" ]] && _zsh_tmux_plugin_run
+fi
+
