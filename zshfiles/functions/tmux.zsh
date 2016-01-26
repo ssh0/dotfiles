41d28eecfedaf00df9a2b94d2af01c3f6d4909c0
Shotaro Fujimoto <fuji101ijuf@gmail.com>
2016-01-26 13:30:58 +0900

    tmux.zsh: Add features: Create new session / Select id with $PERCOL

--- zshfiles/functions/tmux.zsh
+++ zshfiles/functions/tmux.zsh
@@ -11,13 +11,17 @@ alias tkss='tmux kill-session -t'
 if hash tmux; then
   # Autostart if not already in tmux.
   if [[ -z "$TMUX" ]]; then
-    # get the id of a seattached session
-    ID="`tmux ls | grep -vm1 attached | cut -d: -f1`"
+    # get the IDs
+    ID="`tmux ls`"
     if [[ -z "$ID" ]]; then
       tmux new-session && exit
-    else
-      tmux attach-session -d -t "$ID" && exit
     fi
+    ID="$ID\nCreate New Session:"
+    ID="`echo $ID | $PERCOL | cut -d: -f1`"
+    if [[ "$ID" = "Create New Session" ]]; then
+      tmux new-session && exit
+    fi
+    tmux attach-session -d -t "$ID" && exit
   fi
 fi
 
