480b49ec1cca7a5d0bab2c5c9464e5c1256f65cd
Shotaro Fujimoto <fuji101ijuf@gmail.com>
2016-02-06 12:49:24 +0900

    tmux.zsh: Modify - "Create new session" comes first

--- zshfiles/functions/tmux.zsh
+++ zshfiles/functions/tmux.zsh
@@ -1,41 +1,38 @@
 # tmux
 
-# Only run if tmux is actually installed
-if hash tmux; then
-  # Start a new session from within tmux with ZSH_TMUX_AUTOSTART=true - Super User
-  # http://superuser.com/questions/821339/start-a-new-session-from-within-tmux-with-zsh-tmux-autostart-true
-  tmux-new-session() {
-    if [[ -n $TMUX ]]; then
-      tmux switch-client -t "$(TMUX= tmux -S "${TMUX%,*,*}" new-session -dP "$@")"
-    else
-      tmux new-session "$@"
-    fi
-  }
+# Start a new session from within tmux with ZSH_TMUX_AUTOSTART=true - Super User
+# http://superuser.com/questions/821339/start-a-new-session-from-within-tmux-with-zsh-tmux-autostart-true
+tmux-new-session() {
+  if [[ -n $TMUX ]]; then
+    tmux switch-client -t "$(TMUX= tmux -S "${TMUX%,*,*}" new-session -dP "$@")"
+  else
+    tmux new-session "$@"
+  fi
+}
 
-  # Aliases
-  alias ta='tmux attach -t'
-  alias ts='tmux-new-session -s'
-  alias tl='tmux list-sessions'
-  alias tksv='tmux kill-server'
-  alias tkss='tmux kill-session -t'
+# Aliases
+alias ta='tmux attach -t'
+alias ts='tmux-new-session -s'
+alias tl='tmux list-sessions'
+alias tksv='tmux kill-server'
+alias tkss='tmux kill-session -t'
 
-  # Autostart if not already in tmux.
-  if [[ ! -n $TMUX ]]; then
-    # get the IDs
-    ID="`tmux list-sessions`"
-    if [[ -z "$ID" ]]; then
-      tmux new-session && exit
-    fi
-    create_new_session="Create New Session"
-    ID="$ID\n${create_new_session}:"
-    ID="`echo $ID | $PERCOL | cut -d: -f1`"
-    if [[ "$ID" = "${create_new_session}" ]]; then
-      tmux new-session && exit
-    elif [[ -n "$ID" ]]; then
-      tmux attach-session -t "$ID" && exit
-    else
-      :  # Start terminal normally
-    fi
+# Autostart if not already in tmux.
+if [[ ! -n $TMUX ]]; then
+  # get the IDs
+  ID="`tmux list-sessions`"
+  if [[ -z "$ID" ]]; then
+    tmux new-session && exit
+  fi
+  create_new_session="Create New Session"
+  ID="${create_new_session}:\n$ID"
+  ID="`echo $ID | $PERCOL | cut -d: -f1`"
+  if [[ "$ID" = "${create_new_session}" ]]; then
+    tmux new-session && exit
+  elif [[ -n "$ID" ]]; then
+    tmux attach-session -t "$ID" && exit
+  else
+    :  # Start terminal normally
   fi
 fi
 
