261a290f071705692bbb05760750087e6ba678b8
Shotaro Fujimoto <fuji101ijuf@gmail.com>
2016-02-09 19:09:33 +0900

    Modify install.sh

--- install.sh
+++ install.sh
@@ -8,30 +8,39 @@ DOT_DIR="$(builtin cd "$(dirname "${BASH_SOURCE:-${(%):-%N}}")" && pwd)"
 DOT_REPO="https://github.com/ssh0/dotfiles.git"
 DOT_SCR="$HOME/.dot"
 
-echo "Install ssh0's dotfiles ..."
+echo "-------------------------"
+echo " Install ssh0's dotfiles "
+echo "-------------------------"
 echo
-
 echo "First, Clone 'dot' command from GitHub."
+echo "    $ git clone https://github.com/ssh0/dot.git ${DOT_SCR}"
 echo
 git clone https://github.com/ssh0/dot.git "${DOT_SCR}"
 
-echo "Make a temporary configuration file for 'dot'."
-temp_config="$(mktemp)"
+# Source dot command
+if [ -f "${DOT_SCR}/dot.sh" ] && . "${DOT_SCR}/dot.sh"; then
+  echo "Succefully installed 'dot'."
+  echo
+else
+  echo "'dot' command cannot be installed."
+  echo "Try manuall installation."
+  echo "See https://github.com/ssh0/dotfiles#manually"
+  echo
+  exit 1
+fi
 
-echo
-cat << EOF | tee "${temp_config}"
+# Create temporary configuration file to set some variables.
+temp_config="$(mktemp)"
+cat << EOF > "${temp_config}"
 clone_repository='${DOT_REPO}'
 dotdir='${DOT_DIR}'
 EOF
 
-# Source dot command
-. "${DOT_SCR}/dot.sh"
-
 # Ask full install or not
-read -p "You want to install all the symlinks? [y/N]" confirm
+read -p "You want to install **all** the symlinks? [y/N]" confirm
 echo
 if [ "$confirm" != "y" ]; then
-  echo "You can edit 'dotlink' file to toggle which files to be made symlinks."
+  echo "Edit 'dotlink' file to toggle which files to be made symlinks."
   echo "Comment out the lines you don't want to make symlinks."
   echo "(The script asks you when the files already exists. Don't worry about it.)"
   echo
@@ -42,5 +51,7 @@ fi
 
 echo "Now, set the symbolic links."
 echo -n "Press any key to continue ..."; read
+echo
+
 dot_main -c "${temp_config}" set -v
 
