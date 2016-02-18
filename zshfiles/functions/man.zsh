5aeda17d141547ec1319585a4df6126954aa0df4
Shotaro Fujimoto <fuji101ijuf@gmail.com>
2016-02-18 11:18:49 +0900

    man.zsh: It's now for zsh-builtins and alias and functions (with color!)

--- zshfiles/functions/man.zsh
+++ zshfiles/functions/man.zsh
@@ -1,23 +1,52 @@
 # vim: set ft=zsh
 #
-# man (available for buitlin)
-#----------------------------
+# man (available for buitlin and so on)
+#--------------------------------------
 #
 # Can I get individual man pages for the bash builtin commands? - Unix & Linux Stack Exchange
 #   http://unix.stackexchange.com/questions/18087/can-i-get-individual-man-pages-for-the-bash-builtin-commands
 # manpage - How to make `man` work for shell builtin commands and keywords? - Ask Ubuntu
 #   http://askubuntu.com/questions/439410/how-to-make-man-work-for-shell-builtin-commands-and-keywords
+#
+# But I want to know about zsh builtins, so I wrote this.
+#
+# Install zsh manuals to enable `man zshbuiltins`:
+#     $ mkdir -p ~/Downloads/zsh-doc; cd $_
+#     $ wget http://downloads.sourceforge.net/project/zsh/zsh/5.0.2/zsh-5.0.2.tar.bz2
+#     $ tar -xvf zsh-5.0.2.tar.bz2
+#     $ sudo cp Doc/*.1 /usr/local/share/man/man1
+#
+# Require:
+#     * fzf
+#
+# Optional:
+#     * "highlight" for highlighting scripts
+#     * LESS="R" option for ansi color in "less" command
+# 
 
 function man() {
-  case "$(bash -c "type -t -- $1"):$1" in
-    builtin:*|keyword:*) # built-in
-      bash -c ". $HOME/.zsh/functions/less.zsh; help -m $1 | less"
+  case "$(whence -wa -- $1 | uniq | fzf -1 | sed 's/: / /' | cut -d' ' -f2)" in
+    builtin) # built-in
+      /usr/bin/man --pager="less -p'^       \\$1 '" zshbuiltins
+      # TODO: how many spaces before the commands?
       ;;
-    *[[?*]*) # pattern
-      bash -c ". $HOME/.zsh/functions/less.zsh; help -m $1 | less"
+    reserved) # reserved words
+      /usr/bin/man --pager="less -p'^COMPLEX COMMANDS$'" zshall
       ;;
-    *) command -p man "$@";;  # something else, presumed to be an external command
-                              # or options for the man command or a section number
+    alias) # alias
+      whence -c $1
+      ;;
+    function) # function
+      if hash highlight 2>/dev/null; then
+        whence -f "$(whence $1)" \
+          | highlight --out-format=ansi --src-lang=Bash \
+          | ${MANPAGER:-${PAGER:-less}}
+      else
+        whence -f "$(whence $1)" | ${MANPAGER:-${PAGER:-less}}
+      fi
+      ;;
+    *) /usr/bin/man "$@"
+      ;;
   esac
 }
 
