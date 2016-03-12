959ae7adc6d2752b6a21afc9b00b38e8e2dc27fe
Shotaro Fujimoto <fuji101ijuf@gmail.com>
2016-03-12 18:43:05 +0900

    bin/mplayer_term_wrapper.sh: Fix bug

--- bin/mplayer_term_wrapper.sh
+++ bin/mplayer_term_wrapper.sh
@@ -6,4 +6,4 @@
 xterm -fn 6x12 \
       -e "export TERM=xterm-16color; \
           export CACA_DRIVER=ncurses; \
-          mplayer -framedrop -msglevel all=0 -quiet -vo caca $1"
+          mplayer -framedrop -msglevel all=0 -quiet -vo caca \"$1\""
