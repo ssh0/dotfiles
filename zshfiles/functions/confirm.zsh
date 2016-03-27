d219791b16a1ae64f2fd4d6b3c9844a82ce0c5f6
Shotaro Fujimoto <fuji101ijuf@gmail.com>
2016-03-27 20:07:18 +0900

    :new: Add confirm.zsh

---
+++ zshfiles/functions/confirm.zsh
@@ -0,0 +1,81 @@
+#=#=#=
+# Function for asking user yes or no
+#
+# **Features**
+#
+# * can set default answer (default one is shown in caitals)
+# * can set messages
+# * loop if the answer is invalid
+# * ignorecase
+#
+# ```
+# Name
+#       confirm - confirm user yes or no
+#
+# Usage
+#       confirm [ y | n ] [<message>]
+#
+# Examples
+#       Use this function to ask user yes or no.
+#
+#       * Set default answer by setting first argument 'y':
+#
+#           $ confirm y "Choose y or n " && echo "Yes" || echo "No"
+#           Choose y or n (Y/n)>
+#           Yes
+#
+#           $ confirm y "Choose y or n " && echo "Yes" || echo "No"
+#           Choose y or n (Y/n)> n
+#           No
+#
+#       * If the first argument is not 'y' or 'n':
+#
+#           $ confirm "Choose y or n " && echo "Yes" || echo "No"
+#           Choose y or n (y/n)>
+#           Please answer with 'y' or 'n'.
+#           Choose y or n (y/n)> y
+#           Yes
+#
+#       * You can answer with upper characters:
+#
+#           $ confirm "Choose y or n " && echo "Yes" || echo "No"
+#           Choose y or n (y/n)> YeS
+#           Yes
+#
+# ```
+#=#=
+function confirm() {
+  # confirm [ y | n ] [<message>]
+  local yn mes __confirm ret
+  if [ "$1" = "y" ]; then
+    yn="Y/n"
+    mes="$2"
+    ret=0
+  elif [ "$1" = "n" ]; then
+    yn="y/N"
+    mes="$2"
+    ret=1
+  else
+    yn="y/n"
+    mes="$1"
+    ret=-1
+  fi
+
+  echo -n "$mes($yn)> "
+  read __confirm
+  __confirm=$(echo ${__confirm} | tr '[:upper:]' '[:lower:]')
+  case ${__confirm} in
+    y|yes) return 0 ;;
+     n|no) return 1 ;;
+       "") if [ $ret -eq -1 ]; then
+             echo "Please answer with 'y' or 'n'."
+             confirm "$@"
+           else
+             return $ret
+           fi
+           ;;
+        *) echo "Please answer with 'y' or 'n'."
+           confirm $@;;
+  esac
+
+}
