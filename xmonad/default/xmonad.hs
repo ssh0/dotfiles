050de92b14f5c7108ad8d16dafd1da104cf0d4f9
Shotaro Fujimoto <fuji101ijuf@gmail.com>
2016-04-02 17:15:38 +0900

    xmonad.hs: Add new float option for "Figure 1" (matplotlib simulation)

--- xmonad/default/xmonad.hs
+++ xmonad/default/xmonad.hs
@@ -307,6 +307,7 @@ myManageHookFloat = composeAll
     , stringProperty "WM_NAME" =? "LINE" --> (doRectFloat $ W.RationalRect 0.60 0.1 0.39 0.82)
     , stringProperty "WM_NAME" =? "Google Keep" --> (doRectFloat $ W.RationalRect 0.3 0.1 0.4 0.82)
     , stringProperty "WM_NAME" =? "tmptex.pdf - 1/1 (96 dpi)" --> (doRectFloat $ W.RationalRect 0.29 0.25 0.42 0.5)
+    , stringProperty "WM_NAME" =? "Figure 1" --> doCenterFloat
     ]
 
 --------------------------------------------------------------------------- }}}
