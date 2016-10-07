a6100deca8e8bba30b74f3b6c8ab73d2ed742da2
Shotaro Fujimoto <fuji101ijuf@gmail.com>
2016-10-07 18:24:37 +0900

    T540p/xmonad.hs: Modify manageHook - Move 'Figure 1' to workspace 5

--- xmonad/T540p/xmonad.hs
+++ xmonad/T540p/xmonad.hs
@@ -283,6 +283,7 @@ myStartupHook = do
 myManageHookShift = composeAll
             -- if you want to know className, type "$ xprop|grep CLASS" on shell
             [ className =? "Gimp"       --> mydoShift "3"
+            , stringProperty "WM_NAME" =? "Figure 1" --> doShift "5"
             ]
              where mydoShift = doF . liftM2 (.) W.greedyView W.shift
 
@@ -311,6 +312,7 @@ myManageHookFloat = composeAll
     , stringProperty "WM_NAME" =? "LINE" --> (doRectFloat $ W.RationalRect 0.60 0.1 0.39 0.82)
     , stringProperty "WM_NAME" =? "Google Keep" --> (doRectFloat $ W.RationalRect 0.3 0.1 0.4 0.82)
     , stringProperty "WM_NAME" =? "tmptex.pdf - 1/1 (96 dpi)" --> (doRectFloat $ W.RationalRect 0.29 0.25 0.42 0.5)
+    , stringProperty "WM_NAME" =? "Figure 1" --> doFullFloat
     ]
 
 --------------------------------------------------------------------------- }}}
