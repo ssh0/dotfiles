5766e31b4ec9fc23400ef9be7c1769601ce79f32
Shotaro Fujimoto <fuji101ijuf@gmail.com>
2016-12-01 02:26:53 +0900

    less.zsh: Modify highlight setting

--- zshfiles/functions/less.zsh
+++ zshfiles/functions/less.zsh
@@ -9,14 +9,14 @@
 #=#=
 
 # less options:
-export LESS='-iMRj.5'
+export LESS='-iMRgj.5'
 
 # colored less
 export LESS_TERMCAP_mb=$(tput bold)                # begin blinking
 export LESS_TERMCAP_md=$(tput bold; tput setaf 4)  # begin bold (blue)
 export LESS_TERMCAP_me=$(tput sgr0)                # end mode
 export LESS_TERMCAP_se=$(tput sgr0)                # end standout-mode
-export LESS_TERMCAP_so=$(tput setaf 3; tput rev)  # begin standout-mode (yellow)
+export LESS_TERMCAP_so=$(tput setaf 3)             # begin standout-mode (yellow)
 export LESS_TERMCAP_ue=$(tput rmul; tput sgr0)     # end underline
 export LESS_TERMCAP_us=$(tput smul; tput setaf 2)  # begin underline (green)
 
