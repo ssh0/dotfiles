2c1b21d0329995f38bf521e08ba6000980d10f55
Shotaro Fujimoto <fuji101ijuf@gmail.com>
2017-02-15 07:04:12 +0900

    herbstluftwm: Modify focus/bring command

--- herbstluftwm/bring_client_by_name.sh
+++ herbstluftwm/bring_client_by_name.sh
@@ -2,4 +2,7 @@
 # written by Shotaro Fujimoto (https://github.com/ssh0)
 # first edited: 2017-02-15
 
-herbstclient bring "$($HOME/.config/herbstluftwm/pick_clients_hexid_by_window_name.sh)"
+hexid="$($HOME/.config/herbstluftwm/pick_clients_hexid_by_window_name.sh)"
+if [ -n "${hexid}" ]; then
+  herbstclient bring "${hexid}"
+fi
