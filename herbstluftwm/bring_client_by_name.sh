bec6d93a930001037515b0f1d11485547bd6c74a
Shotaro Fujimoto <fuji101ijuf@gmail.com>
2017-02-16 06:26:24 +0900

    herbstluftwm: Clean up

--- herbstluftwm/bring_client_by_name.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# written by Shotaro Fujimoto (https://github.com/ssh0)
-# first edited: 2017-02-15
-
-hexid="$($HOME/.config/herbstluftwm/pick_clients_hexid_by_window_name.sh)"
-if [ -n "${hexid}" ]; then
-  herbstclient bring "${hexid}"
-fi
