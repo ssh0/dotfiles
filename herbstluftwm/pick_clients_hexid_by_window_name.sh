4da5933e299cf412ebee1f89949f2994d0aee820
Shotaro Fujimoto <fuji101ijuf@gmail.com>
2017-02-15 10:57:21 +0900

    Modify text align

--- herbstluftwm/pick_clients_hexid_by_window_name.sh
+++ herbstluftwm/pick_clients_hexid_by_window_name.sh
@@ -5,7 +5,7 @@
 herbstclient object_tree clients \
   | grep -v 'focus' | sed -e '1d' | cut -b7- |
   while read line; do
-    echo -n "[$(herbstclient get_attr .clients.${line}.tag)] "
-    echo -n "$(herbstclient get_attr .clients.${line}.title) "
-    echo "(${line})"
-  done | dmenu -b -f -i -l 10 -fn "Migu 1M:size=20" | rev | cut -b2-10 | rev
+    tag="[$(herbstclient get_attr .clients.${line}.tag)]"
+    title="$(herbstclient get_attr .clients.${line}.title)"
+    printf "%-3s %-120s:${line}\n" "${tag}" "${title}"
+  done | dmenu -b -f -i -l 10 -fn "Migu 1M:size=20" | rev | cut -d: -f1 | rev
