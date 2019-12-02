e262d34f404f9fce747f2cfee720831e0949a333
Shotaro Fujimoto <fuji101ijuf@gmail.com>
2019-12-02 15:17:44 +0900

    Add new files

---
+++ zshfiles/functions/awscli.zsh
@@ -0,0 +1,7 @@
+#=#=#=
+# Set aws cli util
+#
+# ref) https://docs.aws.amazon.com/ja_jp/cli/latest/userguide/cli-configure-completion.html
+#=#=
+# Enable completion
+source /usr/local/bin/aws_zsh_completer.sh
