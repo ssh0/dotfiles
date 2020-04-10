c446a9b91dbfbf9f730edbef000e3e1044e64699
Shotaro Fujimoto <fuji101ijuf@gmail.com>
2020-04-10 20:22:11 +0900

    amixerの仕様変更に対応

--- bin/sound_volume_change_wrapper.sh
+++ bin/sound_volume_change_wrapper.sh
@@ -8,9 +8,9 @@
 
 case "$1" in
   "+")
-    amixer -D pulse set Master 1%+ && paplay /usr/share/sounds/freedesktop/stereo/audio-volume-change.oga &;;
+    amixer set Master 1%+ && paplay /usr/share/sounds/freedesktop/stereo/audio-volume-change.oga &;;
   "-")
-    amixer -D pulse set Master 1%- && paplay /usr/share/sounds/freedesktop/stereo/audio-volume-change.oga &;;
+    amixer set Master 1%- && paplay /usr/share/sounds/freedesktop/stereo/audio-volume-change.oga &;;
   "m")
-    amixer -D pulse set Master toggle ;;
+    amixer set Master toggle ;;
 esac
